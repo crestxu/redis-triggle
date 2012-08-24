211ab01241ca6c07144935b6fde562181aed8a61
crestxu <crestxu@gmail.com>
2012-08-24 16:37:36 +0800

    add lua exec

--- src/baidu_bridge/bridge_process.c
+++ src/baidu_bridge/bridge_process.c
@@ -138,7 +138,7 @@ void triggleGenericCommand(redisClient *c, int nx, robj *db_id, robj *key_patter
    funcdef = sdscatlen(funcdef,"() ",3);
    funcdef = sdscatlen(funcdef,script_source->ptr,sdslen(script_source->ptr));
    funcdef = sdscatlen(funcdef," end",4);
-   redisLog("script function:%s",funcdef->ptr)
+   redisLog(REDIS_NOTICE,"script function:%s",funcdef);
    
    if (luaL_loadbuffer(server.lua,funcdef,sdslen(funcdef),"@user_script")) {
 		   addReplyErrorFormat(c,"Error compiling script (new function): %s\n",
@@ -160,24 +160,6 @@ void triggleGenericCommand(redisClient *c, int nx, robj *db_id, robj *key_patter
 
 
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-	
 	struct bridge_db_triggle_t *tmptrg=malloc(sizeof(struct bridge_db_triggle_t));
 	tmptrg->dbid=id;
 	tmptrg->event=int_event;
@@ -257,13 +239,127 @@ void triggleCommand(redisClient *pc)
     triggleGenericCommand(c,0,c->argv[1],c->argv[2],c->argv[3],c->argv[4]);
 }
 
+void luaReplyToLog(redisClient *c, lua_State *lua) {
+    int t = lua_type(lua,-1);
+
+    switch(t) {
+    case LUA_TSTRING:
+        addReplyBulkCBuffer(c,(char*)lua_tostring(lua,-1),lua_strlen(lua,-1));
+        break;
+    case LUA_TBOOLEAN:
+        addReply(c,lua_toboolean(lua,-1) ? shared.cone : shared.nullbulk);
+        break;
+    case LUA_TNUMBER:
+        addReplyLongLong(c,(long long)lua_tonumber(lua,-1));
+        break;
+    case LUA_TTABLE:
+        /* We need to check if it is an array, an error, or a status reply.
+         * Error are returned as a single element table with 'err' field.
+         * Status replies are returned as single elment table with 'ok' field */
+        lua_pushstring(lua,"err");
+        lua_gettable(lua,-2);
+        t = lua_type(lua,-1);
+        if (t == LUA_TSTRING) {
+            sds err = sdsnew(lua_tostring(lua,-1));
+            sdsmapchars(err,"\r\n","  ",2);
+            addReplySds(c,sdscatprintf(sdsempty(),"-%s\r\n",err));
+            sdsfree(err);
+            lua_pop(lua,2);
+            return;
+        }
+
+        lua_pop(lua,1);
+        lua_pushstring(lua,"ok");
+        lua_gettable(lua,-2);
+        t = lua_type(lua,-1);
+        if (t == LUA_TSTRING) {
+            sds ok = sdsnew(lua_tostring(lua,-1));
+            sdsmapchars(ok,"\r\n","  ",2);
+            addReplySds(c,sdscatprintf(sdsempty(),"+%s\r\n",ok));
+            sdsfree(ok);
+            lua_pop(lua,1);
+        } else {
+            void *replylen = addDeferredMultiBulkLength(c);
+            int j = 1, mbulklen = 0;
+
+            lua_pop(lua,1); /* Discard the 'ok' field value we popped */
+            while(1) {
+                lua_pushnumber(lua,j++);
+                lua_gettable(lua,-2);
+                t = lua_type(lua,-1);
+                if (t == LUA_TNIL) {
+                    lua_pop(lua,1);
+                    break;
+                }
+                luaReplyToRedisReply(c, lua);
+                mbulklen++;
+            }
+            setDeferredMultiBulkLength(c,replylen,mbulklen);
+        }
+        break;
+    default:
+        addReply(c,shared.nullbulk);
+    }
+    lua_pop(lua,1);
+}
+
+int do_delete_event(struct redisClient *c,sds *funcname)
+{
+	luaSetGlobalArray(server.lua,"KEYS",c->argv+1,c->argc-1);
+	
+   // luaSetGlobalArray(server.lua,"ARGV",c->argv+3+numkeys,c->argc-3-numkeys);
+
+    /* Select the right DB in the context of the Lua client */
+    selectDb(server.lua_client,c->db->id);
 
 
+   if (server.lua_time_limit > 0) {
+        lua_sethook(server.lua,luaMaskCountHook,LUA_MASKCOUNT,100000);
+        server.lua_time_start = ustime()/1000;
+    } else {
+        lua_sethook(server.lua,luaMaskCountHook,0,0);
+    }
+
+    /* At this point whatever this script was never seen before or if it was
+     * already defined, we can call it. We have zero arguments and expect
+     * a single return value. */
+    if (lua_pcall(server.lua,0,1,0)) {
+        selectDb(c,server.lua_client->db->id); /* set DB ID from Lua client */
+        addReplyErrorFormat(c,"Error running script (call to %s): %s\n",
+            funcname, lua_tostring(server.lua,-1));
+        lua_pop(server.lua,1);
+        lua_gc(server.lua,LUA_GCCOLLECT,0);
+        return;
+    }
+    selectDb(c,server.lua_client->db->id); /* set DB ID from Lua client */
+   // luaReplyToRedisReply(c,lua);
+    lua_gc(server.lua,LUA_GCSTEP,1);
+   
+}
 
 
 void call_bridge_event(struct redisClient *c,int event_type)
 {
-
+	dictIterator *iter=dictGetIterator(c->db.bridge_db.triggle_scripts);
+    dictEntry *trigs;
+    do{
+    	trigs=dictNext(iter);
+		struct bridge_db_triggle_t * tmptrg=dictGetVal(trigs);
+		if(tmptrg==event_type){ //找到指定的类型事件
+		
+		    //do lua call
+		    //check the keys allows first
+		    switch(event_type){
+				case DELETE_EVENT: 
+					do_delete_event(c,dictGetKey(trigs);)
+					break;
+				default:
+					break;
+				
+		    }
+		}
+    }while(trigs!=NULL);
+	dictRelease(iter);
 
     
 }
