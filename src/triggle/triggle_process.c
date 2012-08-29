2cfd4b662b8631e352a8f3dd144ac3bd31a8e1e2
crestxu <crestxu@gmail.com>
2012-08-29 15:28:46 +0800

    fold triggle add

---
+++ src/triggle/triggle_process.c
@@ -0,0 +1,368 @@
+#include"redis.h"
+#include"dict.h"
+
+#include <lua.h>
+#include <lauxlib.h>
+#include <lualib.h>
+#include <ctype.h>
+#include <math.h>
+
+extern struct redisServer server;
+extern struct dictType keyptrDictType;
+
+unsigned int dictSdsHash(const void *key);
+int dictSdsKeyCompare(void *privdata, const void *key1, const void *key2);
+void dictSdsDestructor(void *privdata, void *val);
+void luaMaskCountHook(lua_State *lua, lua_Debug *ar);
+
+void dicttriggleDestructor(void *privdata, void *val)
+{
+    DICT_NOTUSED(privdata);
+
+    struct bridge_db_triggle_t *tmptrg=(struct bridge_db_triggle_t *)val;
+    decrRefCount(tmptrg->lua_scripts);
+    free(val);
+}
+
+
+dictType keytriggleDictType = {
+    dictSdsHash,               /* hash function */
+    NULL,                      /* key dup */
+    NULL,                      /* val dup */
+    dictSdsKeyCompare,         /* key compare */
+    dictSdsDestructor,                      /* key destructor */
+    dicttriggleDestructor                       /* val destructor */
+};
+
+
+
+
+void init_bridge_server()
+{
+
+    int j;
+
+    for (j = 0; j < server.dbnum; j++) {
+
+        server.db[j].bridge_db.bridge_event= BRIDGE_DEFAULT_EVENT;
+        server.db[j].bridge_db.triggle_scipts= dictCreate(&keytriggleDictType,NULL);
+
+
+    }
+
+}
+
+void decrRefTriggleCount(void *obj) {
+    robj *o = obj;
+
+    if (o->refcount <= 0) redisPanic("decrRefCount against refcount <= 0");
+    if (o->refcount == 1) {
+        switch(o->type) {
+            case REDIS_STRING: freeStringObject(o); break;
+            case REDIS_LIST: freeListObject(o); break;
+            case REDIS_SET: freeSetObject(o); break;
+            case REDIS_ZSET: freeZsetObject(o); break;
+            case REDIS_HASH: freeHashObject(o); break;
+            default: redisPanic("Unknown object type"); break;
+        }
+        zfree(o);
+    } else {
+        o->refcount--;
+    }
+}
+
+
+robj *createTriggleObject(void) {
+    list *l = listCreate();
+    robj *o = createObject(REDIS_LIST,l);
+    listSetFreeMethod(l,decrRefTriggleCount);
+    o->encoding = REDIS_ENCODING_RAW;
+    return o;
+}
+
+
+void do_bridge_notify(void  *pdb,void *pkeyobj)
+{
+    redisDb *db=(redisDb *)pdb;
+    robj *keyobj=(robj *)pkeyobj;
+    if(db->bridge_db.bridge_event==BRIDGE_KEY_NOTIFY) //do notify event
+    {
+        sds key = sdsnew(BRIDGE_SYSTEM_CHANNEL);
+        robj *bridge_channel = createStringObject(key,sdslen(key));
+        int receivers = pubsubPublishMessage(bridge_channel,keyobj);
+        if (server.cluster_enabled) clusterPropagatePublish(bridge_channel,keyobj);
+        redisLog(REDIS_NOTICE,"%d clients receive the expire event",receivers);
+        decrRefCount(bridge_channel);
+    }
+}
+
+
+
+void triggleGenericCommand(redisClient *c, int nx, robj *db_id, robj *key_pattern,robj *event_type, robj *script_source) {
+    redisLog(REDIS_NOTICE,"dbid: %s keypattern: %s script_source: %s ",db_id->ptr,key_pattern->ptr,script_source->ptr);
+    int id = atoi(db_id->ptr);
+    int int_event=atoi(event_type->ptr);
+    if(id<0||id>server.dbnum)
+    {
+        addReplyError(c,"wrong dbid for triggle");
+        return;
+    }
+
+
+    /*lua_check*/
+    sds funcdef = sdsempty();
+
+    funcdef = sdscat(funcdef,"function ");
+    funcdef = sdscatlen(funcdef,key_pattern->ptr,sdslen(key_pattern->ptr));
+    funcdef = sdscatlen(funcdef,"() ",3);
+    funcdef = sdscatlen(funcdef,script_source->ptr,sdslen(script_source->ptr));
+    funcdef = sdscatlen(funcdef," end",4);
+    redisLog(REDIS_NOTICE,"script function:%s",funcdef);
+
+    if (luaL_loadbuffer(server.lua,funcdef,sdslen(funcdef),"@user_script")) {
+        addReplyErrorFormat(c,"Error compiling script (new function): %s\n",
+                lua_tostring(server.lua,-1));
+        lua_pop(server.lua,1);
+        sdsfree(funcdef);
+        return ;
+    }
+    sdsfree(funcdef);
+    if (lua_pcall(server.lua,0,0,0)) {
+        addReplyErrorFormat(c,"Error running script (new function): %s\n",
+                lua_tostring(server.lua,-1));
+        lua_pop(server.lua,1);
+        return ;
+    }
+
+
+    /*end lua check*/
+
+
+
+    struct bridge_db_triggle_t *tmptrg=malloc(sizeof(struct bridge_db_triggle_t));
+    tmptrg->dbid=id;
+    tmptrg->event=int_event;
+    tmptrg->lua_scripts=script_source;
+    incrRefCount(script_source);
+    sds copy=sdsdup(key_pattern->ptr);
+    dictAdd(server.db[id].bridge_db.triggle_scipts,copy,tmptrg);
+    addReply(c, nx ? shared.cone : shared.ok);
+
+
+}
+
+void triggleDelCommand(struct redisClient *c)
+{
+    int id = atoi(c->argv[1]->ptr);
+    if(id<0||id>server.dbnum)
+    {
+        addReplyError(c,"wrong dbid for triggle");
+        return;
+    }
+    if(	dictDelete(server.db[id].bridge_db.triggle_scipts,c->argv[2]->ptr)==DICT_OK)
+    {
+        addReply(c,  shared.ok);
+
+    }
+    else
+    {
+        addReplyError(c,"delete unknow error");
+    }
+
+}
+
+
+void triggleListCommand(struct redisClient *c)
+{
+
+    int id = atoi(c->argv[1]->ptr);
+    if(id<0||id>server.dbnum)
+    {
+        addReplyError(c,"wrong dbid for triggle");
+        return;
+    }
+   redisLog(REDIS_NOTICE,"dbid:%d key:%s",id,c->argv[2]->ptr);
+    struct dictEntry *de = dictFind(server.db[id].bridge_db.triggle_scipts,c->argv[2]->ptr);
+    if(de)
+    {
+
+        struct bridge_db_triggle_t * tmptrg=dictGetVal(de);
+
+        addReplyStatusFormat(c,"dbid:%dkey:%sevent:%d source:%s",tmptrg->dbid,(char *)c->argv[2]->ptr,tmptrg->event,(char *)tmptrg->lua_scripts->ptr);
+
+    }
+    else
+    {
+        addReplyError(c,"triggle not found");
+    }
+
+}
+
+
+void triggleCommand(redisClient *pc)
+{
+
+    redisClient *c=(redisClient *)pc;
+
+    triggleGenericCommand(c,0,c->argv[1],c->argv[2],c->argv[3],c->argv[4]);
+}
+
+
+int do_delete_event(struct redisClient *c,sds *funcname)
+{
+
+    redisSrand48(0);
+    server.lua_random_dirty = 0;
+    server.lua_write_dirty = 0;
+    lua_getglobal(server.lua,(char *)funcname);
+    
+
+
+    if (lua_isnil(server.lua,1)) {
+        addReplyError(c,"no funcname triggle_scipts in lua");
+        return 0;
+    }
+    luaSetGlobalArray(server.lua,"KEYS",c->argv+1,c->argc-1);
+
+    redisLog(REDIS_NOTICE,"stack: %d",lua_gettop(server.lua));
+ 
+
+    #ifdef BRIDGE_DEBUG
+    for(int i=0;i<c->argc-1;i++){
+        redisLog(REDIS_NOTICE,"%s",(c->argv+1)[i]->ptr);
+    }
+    #endif
+
+
+    /* Select the right DB in the context of the Lua client */
+    selectDb(server.lua_client,c->db->id);
+
+
+    server.lua_time_start = ustime()/1000;
+    server.lua_kill = 0;
+
+    if (server.lua_time_limit > 0) {
+        lua_sethook(server.lua,luaMaskCountHook,LUA_MASKCOUNT,100000);
+        server.lua_time_start = ustime()/1000;
+    } else {
+        lua_sethook(server.lua,luaMaskCountHook,0,0);
+    }
+
+    /* At this point whatever this script was never seen before or if it was
+     * already defined, we can call it. We have zero arguments and expect
+     * a single return value. */
+    if (lua_pcall(server.lua,0,1,0)) {
+        selectDb(c,server.lua_client->db->id); /* set DB ID from Lua client */
+        addReplyErrorFormat(c,"Error running script (call to %s): %s\n",
+                (char*)funcname, lua_tostring(server.lua,-1));
+        lua_pop(server.lua,1);
+        lua_gc(server.lua,LUA_GCCOLLECT,0);
+        return -1;
+    }
+    selectDb(c,server.lua_client->db->id); /* set DB ID from Lua client */
+   // luaReplyToRedisReply(c,server.lua);
+    server.lua_timedout = 0;
+    server.lua_caller = NULL;
+    lua_gc(server.lua,LUA_GCSTEP,1);
+
+    //for slaves
+    //
+
+    return 0; 
+}
+
+
+int triggle_event(struct redisClient *c,sds *funcname)
+{
+
+    redisSrand48(0);
+    server.lua_random_dirty = 0;
+    server.lua_write_dirty = 0;
+    lua_getglobal(server.lua,(char *)funcname);
+    
+
+
+    if (lua_isnil(server.lua,1)) {
+        addReplyError(c,"no funcname triggle_scipts in lua");
+        return 0;
+    }
+    luaSetGlobalArray(server.lua,"KEYS",c->argv+1,c->argc-1);
+
+    redisLog(REDIS_NOTICE,"stack: %d",lua_gettop(server.lua));
+ 
+
+    #ifdef BRIDGE_DEBUG
+    for(int i=0;i<c->argc-1;i++){
+        redisLog(REDIS_NOTICE,"%s",(c->argv+1)[i]->ptr);
+    }
+    #endif
+
+
+    /* Select the right DB in the context of the Lua client */
+    selectDb(server.lua_client,c->db->id);
+
+
+    server.lua_time_start = ustime()/1000;
+    server.lua_kill = 0;
+
+    if (server.lua_time_limit > 0) {
+        lua_sethook(server.lua,luaMaskCountHook,LUA_MASKCOUNT,100000);
+        server.lua_time_start = ustime()/1000;
+    } else {
+        lua_sethook(server.lua,luaMaskCountHook,0,0);
+    }
+
+    /* At this point whatever this script was never seen before or if it was
+     * already defined, we can call it. We have zero arguments and expect
+     * a single return value. */
+    if (lua_pcall(server.lua,0,1,0)) {
+        selectDb(c,server.lua_client->db->id); /* set DB ID from Lua client */
+        addReplyErrorFormat(c,"Error running script (call to %s): %s\n",
+                (char*)funcname, lua_tostring(server.lua,-1));
+        lua_pop(server.lua,1);
+        lua_gc(server.lua,LUA_GCCOLLECT,0);
+        return -1;
+    }
+    selectDb(c,server.lua_client->db->id); /* set DB ID from Lua client */
+   // luaReplyToRedisReply(c,server.lua);
+    server.lua_timedout = 0;
+    server.lua_caller = NULL;
+    lua_gc(server.lua,LUA_GCSTEP,1);
+
+    //for slaves
+    //
+
+    return 0; 
+}
+
+void call_bridge_event(struct redisClient *c,int event_type)
+{
+    struct dictIterator *iter=dictGetIterator(c->db->bridge_db.triggle_scipts);
+    dictEntry *trigs;
+    do{
+        trigs=dictNext(iter);
+        if(trigs!=NULL)
+        {
+            struct bridge_db_triggle_t * tmptrg=dictGetVal(trigs);
+            if(tmptrg->event==event_type){ //找到指定的类型事件
+
+            }
+        }
+    }while(trigs!=NULL);
+    dictReleaseIterator(iter);
+
+
+}
+void call_bridge_event_before(struct redisClient *c,int event_type)
+{
+    call_bridge_event(c,event_type);
+}
+
+
+void call_bridge_event_after(struct redisClient *c,int event_type)
+{
+
+    call_bridge_event(c,event_type);
+}
+
+
